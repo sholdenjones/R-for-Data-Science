d9c74a2756c52ba9dd3930462023f64e2aee9aff
S. Holden Jones <109999931+sholdenjones@users.noreply.github.com>
2022-07-27 09:28:28 -0500

    Add files via upload

--- Ch04_lab.R
+++ Ch04_lab.R
@@ -6,7 +6,7 @@ library(tidyverse) #loads the ggplot2 package. Could also use library(ggplot2)
 #https://r4ds.had.co.nz/workflow-basics.html#exercises-7
 ##########################################################
 #1. Why does this code not work?
-my_variable <- 10
+(my_variable <- 10) # fixed 1 to i in variable
 
 # cool! surrounding a call with () automatically prints it out, can save time and space this way
 
@@ -21,9 +21,3 @@ filter(diamonds, carat > 3)
 #3.  Press Alt + Shift + K (Option+Shift+K on a mac). What happens? How can you get to the same place using the menus? Find the Execute section. What is the keyboard shortcut for sourcing  (i.e., run the whole script) a file?
 # brings up a massive shortcut menu, right on!
 # sourcing shortcut is 
-
-
-
-
-
-
